b86f60e65922444790db2abec0a553362380c8d4
Christian Borowski <c.borowski@web.de>
2011-10-06 06:39:59 +0200

    change keycodes from SHIFT to SHIFT+ALT

--- client/key.js
+++ client/key.js
@@ -2,20 +2,20 @@
 //
 // Bindings from: pvilchez@github
 //
-// Shift+N   : Creates a new card and clears the text for you.
-// Shift+=   : Creates a new column and clears the title for you.
-// Shift+-   : Deletes the newest column.
-// Shift+A   : Changes the theme to 'bigcards'
-// Shift+Z   : Changes the theme to 'smallcards'
+// Shift+ALT+N   : Creates a new card and clears the text for you.
+// Shift+ALT+=   : Creates a new column and clears the title for you.
+// Shift+ALT+-   : Deletes the newest column.
+// Shift+ALT+A   : Changes the theme to 'bigcards'
+// Shift+ALT+Z   : Changes the theme to 'smallcards'
 //
 // Bindings from: borinsky@github
 //
-// Shift+Y	 : Creates a new YELLOW card and clears the text for you. 
-// Shift+G	 : Creates a new GREEN card and clears the text for you.
-// Shift+B	 : Creates a new BLUE and clears the text for you.
-// Shift+W	 : Creates a new WHITE card and clears the text for you.
+// Shift+ALT+Y	 : Creates a new YELLOW card and clears the text for you. 
+// Shift+ALT+G	 : Creates a new GREEN card and clears the text for you.
+// Shift+ALT+B	 : Creates a new BLUE and clears the text for you.
+// Shift+ALT+W	 : Creates a new WHITE card and clears the text for you.
 //
-// todo: Shift+C     : Create a new CODER_CARD - CODERCARD is a Card for a CODER 
+// todo: Shift+ALT+C     : Create a new CODER_CARD - CODERCARD is a Card for a CODER 
 //				       so you can see who is doing the Job
 //				
 // 
@@ -24,7 +24,7 @@ $(document).ready(function () {
         var direction = null;
 
         //Shift+N: Creates a new card and clears the text for you.
-        if (event.keyCode == '78' && event.shiftKey){
+        if (event.keyCode == '78' && event.shiftKey && event.altKey ){
             var rotation = Math.random() * 10 - 5; //add a bit of random rotation (+/- 10deg)
             uniqueID = Math.round(Math.random()*99999999);
             createCard('card' + uniqueID, '', 58, 460, rotation, randomCardColour());
@@ -34,7 +34,7 @@ $(document).ready(function () {
         }
 
         //Shift+Y: Creates a new YELLOW card and clears the text for you.
-        if (event.keyCode == '89' && event.shiftKey){
+        if (event.keyCode == '89' && event.shiftKey && event.altKey ){
             var rotation = Math.random() * 10 - 5; //add a bit of random rotation (+/- 10deg)
             uniqueID = Math.round(Math.random()*99999999);
             createCard('card' + uniqueID, '', 58, 460, rotation, 'yellow' );
@@ -44,7 +44,7 @@ $(document).ready(function () {
         }
 
         //Shift+G: Creates a new GREEN card and clears the text for you.
-        if (event.keyCode == '71' && event.shiftKey){
+        if (event.keyCode == '71' && event.shiftKey && event.altKey ){
             var rotation = Math.random() * 10 - 5; //add a bit of random rotation (+/- 10deg)
             uniqueID = Math.round(Math.random()*99999999);
             createCard('card' + uniqueID, '', 58, 460, rotation, 'green' );
@@ -54,7 +54,7 @@ $(document).ready(function () {
         }
 
         //Shift+B: Creates a new BLUE card and clears the text for you.
-        if (event.keyCode == '66' && event.shiftKey){
+        if (event.keyCode == '66' && event.shiftKey && event.altKey ){
             var rotation = Math.random() * 10 - 5; //add a bit of random rotation (+/- 10deg)
             uniqueID = Math.round(Math.random()*99999999);
             createCard('card' + uniqueID, '', 58, 460, rotation, 'blue' );
@@ -65,7 +65,7 @@ $(document).ready(function () {
 
 
         //Shift+W: Creates a new WHITE card and clears the text for you.
-        if (event.keyCode == '87' && event.shiftKey){
+        if (event.keyCode == '87' && event.shiftKey && event.altKey ){
             var rotation = Math.random() * 10 - 5; //add a bit of random rotation (+/- 10deg)
             uniqueID = Math.round(Math.random()*99999999);
             createCard('card' + uniqueID, '', 58, 460, rotation, 'white' );
@@ -77,7 +77,7 @@ $(document).ready(function () {
 
         //Shift+=: Creates a new column and clears the title for you.
         //Works in Chrome/Safari; Doesn't work in FF4 (all osx)
-        else if (event.keyCode == 187 && event.shiftKey) {
+        else if (event.keyCode == 187 && event.shiftKey && event.altKey ) {
             createColumn('New');
 
             //If using the keyboard shortcut, automatically focus on the textbox
@@ -93,13 +93,13 @@ $(document).ready(function () {
         }
 
         //Shift+A: Changes the theme to 'bigcards'
-        else if (event.keyCode == 65 && event.shiftKey) {
+        else if (event.keyCode == 65 && event.shiftKey && event.altKey ) {
             changeThemeTo('bigcards');
             sendAction('changeTheme', "bigcards");
         }
 
         //Shift+Z: Changes the theme to 'smallcards'
-        else if (event.keyCode == 90 && event.shiftKey) {
+        else if (event.keyCode == 90 && event.shiftKey && event.altKey ) {
             changeThemeTo('smallcards');
             sendAction('changeTheme', "smallcards");
         }
